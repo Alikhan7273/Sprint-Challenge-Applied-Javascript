f1aa2eb2588d08618890bd05d813619d9a635353
unknown <Alikhan7273@gmail.com>
2019-07-21 23:42:34 -0500

    done

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,91 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+
+
+
+
+
+let url1 = 'https://lambda-times-backend.herokuapp.com/articles';
+
+function getArticleData (url1) {
+
+    // Get article data from url
+    axios.get(url1)
+    .then( response => {
+
+          // Get articles  
+          let articleContent = []; 
+          articleContent = response["data"]["articles"];      
+
+          let articleName = "javascript";
+          createArticle(articleContent,articleName);
+
+          articleName = "bootstrap";
+          createArticle(articleContent,articleName);
+
+          articleName = "technology";
+          createArticle(articleContent,articleName);
+
+          articleName = "jquery";
+          createArticle(articleContent,articleName);
+
+          articleName = "node.js";
+          createArticle(articleContent,articleName);
+    })
+};
+
+function createArticle(articleContent,articleName) {
+
+    let x = "headline";  
+    let y = "authorName";
+    let z = "authorPhoto";
+
+    for(i in articleContent[articleName]) {       
+
+        // Get the Main DIV 
+        let mainContainerDiv = document.getElementsByClassName("cards-container")[0];
+
+        // Create 4 DIVS and assign Class Names
+        let newDiv1 = document.createElement('div');
+        newDiv1.classList.add('card'); 
+
+        let newDiv2 = document.createElement('div');
+        newDiv2.classList.add('headline'); 
+        newDiv2.innerText = articleContent[articleName][i][x];
+
+        let newDiv3 = document.createElement('div');
+        newDiv3.classList.add('author'); 
+        newDiv3.innerText = articleContent[articleName][i][y];
+
+        let newDiv4 = document.createElement('div');
+        newDiv4.classList.add('img-container');
+
+        // Create a SPAN
+        let newSpan = document.createElement('span');
+        // Create an IMG
+        let newImg =  document.createElement('img');
+        newImg.src = articleContent[articleName][i][z];    
+
+        // Put them together, like lego blocks.
+        // Add the image to the image container DIV
+        newDiv4.appendChild(newImg);
+
+        // Add image container and the span to author        
+        newDiv3.appendChild(newDiv4);
+        newDiv3.appendChild(newSpan);
+
+        // add author to headline
+        newDiv2.appendChild(newDiv3);
+
+        // add headline to card
+        newDiv1.appendChild(newDiv2);
+
+        // add card to cards-container
+        mainContainerDiv.appendChild(newDiv1);
+
+       }
+}
+
+getArticleData(url1);
--- components/Header/index.js
+++ components/Header/index.js
@@ -3,10 +3,78 @@
 // Using a function create the component you see below:
 //
 //  <div class="header">
-//    <span class="date">SMARCH 28, 2019</span>
+//    <span class="date">MARCH 28, 2019</span>
 //    <h1>Lambda Times</h1>
 //    <span class="temp">98°</span>
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+
+function Header() {
+
+    // Get the Header Container Element
+    const headerContainer = document.getElementsByClassName("header-container")[0];
+
+    // create a new DIV
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('header');
+
+    // Create a new SPAN
+    const newSpan1 = document.createElement('span');
+    newSpan1.classList.add('date');
+    newSpan1.innerText = 'MARCH 28, 2019';
+
+    // Create a new H1
+    const newH1 = document.createElement('h1');
+    newH1.innerText = 'Lambda Times';
+
+    // Create a new SPAN
+    const newSpan2 = document.createElement('span');
+    newSpan2.classList.add('temp'); 
+    newSpan2.innerText = '98°';
+
+    // Append all child elements to the new DIV
+    newDiv.appendChild(newSpan1);
+    newDiv.appendChild(newH1);
+    newDiv.appendChild(newSpan2);
+
+    // Append the new DIV to the Header
+    headerContainer.appendChild(newDiv);
+
+}
+
+// Execute Header to create the header
+Header();
+
+
+
+
+
+
+
+// const header = document.querySelector('.headerContainer')
+
+// function Header(date, title,temp) {
+//     // define new elements
+//     const header = document.createElement('div');//className
+//     const span1 = document.createElement('span');//className
+//     const NewH1 = document.createElement('h1');
+//     const span2 = document.createElement('span');//className
+
+//     // Setup structure of elements
+//     header.appendChild(span1)
+//     header.appendChild(NewH1)
+//     header.appendChild(span2)
+
+//     // set class names
+//     header.classList.add('header')
+//     span1.classList.add('date')
+//     span2.classList.add('temp')
+
+        //set textContent
+//     span1.textContent = 'MARCH 28, 2019'
+//     newH1.textContent = 'Lambda Times'
+//     span1.textContent = '98°'
+
+//     return header;
+//}
